0af583f0e9eb344c401328cd443709689aa3eb49
michaekong <michaelndekebai@gmail.com>
2024-12-28 18:36:08 +0100

    urls

--- MEMOCLOUDPROJECT/urls.py
+++ MEMOCLOUDPROJECT/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from memoire.views import *
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
