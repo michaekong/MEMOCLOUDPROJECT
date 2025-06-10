165be9c3c4e95cc8f5549c2829b459e81a2ecaad
michaekong <michaelndekebai@gmail.com>
2025-06-10 03:02:57 +0100

    DATA BASE

--- MEMOCLOUDPROJECT/settings.py
+++ MEMOCLOUDPROJECT/settings.py
@@ -102,15 +102,15 @@ DATABASES = {
 
 
 
-SITE_URL = "https://memocloudproject-qi77.onrender.com"
+SITE_URL = "https://memocloudproject-ezrh.onrender.com"
 """
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-"""
+}"""
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
